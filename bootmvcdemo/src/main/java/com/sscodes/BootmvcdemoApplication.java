d91595eccc74383779e85782f02d2e2ea0a05cd6
SAM <samjoshi.sharma@gmail.com>
2018-06-08 08:20:40 +0530

    Inital commit

---
+++ bootmvcdemo/src/main/java/com/sscodes/BootmvcdemoApplication.java
@@ -0,0 +1,12 @@
+package com.sscodes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootmvcdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootmvcdemoApplication.class, args);
+	}
+}
